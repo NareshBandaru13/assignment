379f728656287639f591d7e2d4d52cdc29095c2d
NareshBandaru13 <102959920+NareshBandaru13@users.noreply.github.com>
2022-04-19 12:56:16 +0530

    Add files via upload

---
+++ code/vector.py
@@ -0,0 +1,9 @@
+#we have,p = [k(N)+1(M)]/(k+1) = 3e1 +2e2
+#also M and N are vectors in e1 and e2 only respectively.
+e1=vector(1,0,0)
+e2=vector(0,1,0)
+k=2/3
+M=3*(k+1)*e1
+N=2*(k+1)*e2/k
+print(M)
+print(N) 
